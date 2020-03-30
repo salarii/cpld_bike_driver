5b99cb8a5fb6c055acf000dae4afcc1d16bf0238
Artur Swiderski <you@example.com>
2020-03-30 05:49:37 +0200

    cont.

--- logic/adc_control.vhd
+++ logic/adc_control.vhd
@@ -13,6 +13,11 @@ entity adc_control is
 		clk : in  std_logic;
 		res : in  std_logic;
 		i2c : inout  transaction_data;
+		
+		o_uart : out unsigned(7 downto 0);
+		busy_uart : in std_logic;
+		en_uart : out std_logic;
+		
 		ready : in  std_logic
 		);
 end adc_control;
@@ -20,17 +25,20 @@ end adc_control;
 architecture behaviour of adc_control is
 		signal data : std_logic_vector(15 downto 0);	
 		signal address : unsigned(6 downto 0) := "1001000";
+		signal enable_uart  : std_logic;
+		signal out_uart : unsigned(7 downto 0);
 begin	
 
 
 	process(clk)
-		type state_type is (Setup, Active);
+		type state_type is (Setup, Standby, Cycle);
 		
 		constant config_register_h : unsigned(7 downto 0) := "11111111";
 		constant config_register_l : unsigned(7 downto 0) := "00001111";
-		variable state : state_type := Active;
-		variable cnt : integer;	
-
+		variable state : state_type := Setup;
+		variable cnt : integer := 0;		
+		variable time : unsigned(15 downto 0);		
+		variable val_cnt : integer  range 3 downto 0 := 0;
 	begin
 	
 		if rising_edge(clk)  then
@@ -50,7 +58,7 @@ begin
 					data(7 downto 0) <= std_logic_vector(config_register_l);
 					
 					if i2c.done = '1' then
-						state := Active;
+						state := Standby;
 						data <=  (others=>'Z');	
 						i2c.enable <= '0';				
 					end if;
@@ -61,37 +69,67 @@ begin
 					end if;
 				
 					cnt := period;
-				elsif state = Active then		
+				elsif state = Standby then		
 						
-					i2c.transaction <= Read;
-					i2c.enable <= '1';
-					i2c.reg_addr <= "11";
-				
-					if i2c.done = '1' then
-						i2c.enable <= '0';				
+					if cnt = 0 then
+						state := Cycle;
+						i2c.transaction <= Read;
+						i2c.enable <= '1';
+						i2c.reg_addr <= "11";
+					
+						if i2c.done = '1' then
+							i2c.enable <= '0';				
+						end if;
+						
+						if i2c.busy = '1' then
+							i2c.enable <= '0';
+	
+						end if;
+					
+					
+						cnt := period;
 					end if;
 					
-					if i2c.busy = '1' then
-						i2c.enable <= '0';
-
-					end if;
-				
-					cnt := period;
-						
 					cnt := cnt - 1;	
-					if cnt = period then
-
-					end if;
-									
+					
+				elsif state = Cycle then
+					
+					
+					if busy_uart = '0' then
+						
+						case val_cnt is
+						  when 0 =>   out_uart <= unsigned(data(15 downto 8));
+						  when 1 =>   out_uart <= unsigned(data(7 downto 0));
+						  when 2 =>   out_uart <= time(15 downto 8);
+						  when 3 =>   out_uart <= time(7 downto 0);
+						  when others => out_uart <=  (others=>'0');
+						end case;
+					
+						if val_cnt = 3 then
+							val_cnt := 0;
+						else
+							enable_uart <= '1';
+							val_cnt := val_cnt + 1;
+						end if;
+					else
+						enable_uart <= '0';
+							
+					end if;			
 				end if;		
-	
+
 			end if;
 
 		end if;
 
 	end process;
 	
-	i2c.data <= data;
-	i2c.address <= std_logic_vector(address);
+	process(out_uart, data, address, enable_uart )
+	begin
+		en_uart <= enable_uart;
+		o_uart <= out_uart;
+		i2c.data <= data;
+		i2c.address <= std_logic_vector(address);	
+	end process;
+
 	
 end behaviour;
