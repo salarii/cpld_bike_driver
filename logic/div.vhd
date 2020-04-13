c25867907e47c80346f9c65e752c9e7043e04b10
salarii <ari>
2020-04-13 16:16:27 +0200

    cont.

--- logic/div.vhd
+++ logic/div.vhd
@@ -5,7 +5,7 @@ use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity div is
-	generic (CONSTANT size : integer := 8);
+	generic (CONSTANT size : integer := 12);
 
 	port(
 		res : in std_logic;
@@ -21,17 +21,15 @@ end div;
 
 architecture behaviour of div is
 	
-		signal tmp : unsigned (size - 1  downto 0);
-
-		signal part_divident : signed (size downto 0 ) := (others => '0'); 
-		signal sig_divisor : signed (size downto 0 ) := (others => '0'); 
 begin	
 
 
 	process(clk)
 		variable  initiated : boolean :=  false;
 		variable index : integer range size-1 downto 0 := 0;
-
+		variable part_divident : signed (size downto 0 ) := (others => '0'); 
+		variable sig_divisor : signed (size downto 0 ) := (others => '0'); 
+		variable tmp : unsigned (size  downto 0);
 	begin
 	
 
@@ -40,25 +38,27 @@ begin
 			if  res = '0' then
 				index := size -1;
 				initiated := false;	
-				part_divident <= (others => '0');
-				sig_divisor <= (others => '0'); 
+				part_divident := (others => '0');
+				sig_divisor := (others => '0'); 
 				o_quotient <= (others => '0'); 
-			elsif i_enable = '1' then 
+				
+			elsif i_enable = '1' or initiated = true then 
 				if initiated = false then
 					index := size -1;
 				
 					initiated := true;
-					tmp <= i_divident;
+					tmp(size -1  downto 0) := i_divident;
+					tmp(size) := '0';
 					o_valid <= '0';
 					o_quotient <= (others => '0'); 
 				else 
 					
-					part_divident(size -1  downto 0) <= signed(shift_right(tmp, index));
-					sig_divisor(size -1  downto 0) <= signed(i_divisor);
-					part_divident <= part_divident - sig_divisor; 
-					if part_divident > 0 then
+					part_divident(size  downto 0) := signed(shift_right(tmp, index));
+					sig_divisor(size -1  downto 0) := signed(i_divisor);
+					part_divident := part_divident - sig_divisor; 
+					if part_divident >= 0 then
 						o_quotient(index) <= '1';
-						tmp(size -1  downto index) <= unsigned(part_divident(size -1  downto index));
+						tmp(size -1  downto index) := unsigned(part_divident(size -1 - index  downto 0));
 					end if;
 				
 					if index = 0 then
