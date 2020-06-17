13464655407b27109c4dee6b30f851096b718404
Artur Swiderski <you@example.com>
2020-06-17 19:23:47 +0200

    working on  controller

--- logic/control_box.vhd
+++ logic/control_box.vhd
@@ -15,6 +15,9 @@ entity control_box is
 		clk : in  std_logic;
 		res : in  std_logic;
 		
+		i_temp_transistors : in unsigned(15 downto 0);
+		i_req_speed : in unsigned(7 downto 0);
+		i_req_temperature : in unsigned(7 downto 0);
 		i_control_box_setup : in type_control_box_setup;
 		i_hal_data : in std_logic_vector(2 downto 0);
 		o_motor_transistors : out type_motor_transistors
@@ -77,6 +80,18 @@ architecture behaviour of control_box is
 				);
 		end component;
 
+		component pd is
+			generic (CONSTANT IntPart : integer := 8;
+		   			 CONSTANT FracPart : integer := 8);
+		
+			port(
+				res : in std_logic;
+				clk : in std_logic;
+				i_enable : in std_logic;
+				i_val	: in  signed(IntPart + FracPart -1  downto 0);
+				o_reg : out signed(IntPart + FracPart -1  downto 0)
+				);
+		end component pd;
 
 		component motor_driver is
 		
@@ -123,46 +138,92 @@ architecture behaviour of control_box is
 
 		constant IntPart : integer := 8;
 		constant FracPart : integer := 8;
+		constant upper_limit : integer := IntPart + FracPart - 1;
 		
-		signal data : std_logic_vector(15 downto 0);	
+		
+		signal data : std_logic_vector(upper_limit downto 0);	
 		
 		signal poly_enable : std_logic;	
 		signal poly_temperature : unsigned(7  downto 0);	
-		signal poly_val : unsigned(15 downto 0) := (others => '0');
+		signal poly_val : unsigned(upper_limit downto 0) := (others => '0');
 
 		signal en_trigger : std_logic;	
 		signal out_trigger : std_logic;
-		signal time_trigger : unsigned(15  downto 0);
-		signal pulse_trigger : unsigned(15 downto 0);	
+		signal time_trigger : unsigned(upper_limit  downto 0);
+		signal pulse_trigger : unsigned(upper_limit downto 0) := (others => '0');	
 		signal stop_trigger : std_logic;
-		
+
+
+		signal enable_pid : std_logic;
+		--signal enable_pd : std_logic;
 		signal enable_div : std_logic;
 		signal valid : std_logic;
 		
 		
 		signal impulse : std_logic;
 		
-		constant battery_voltage : unsigned(15 downto 0) := x"0000";--36V
-		constant offset_voltage : unsigned(15 downto 0) := x"0000";--3V 
 
-		constant period_trigger : unsigned(15 downto 0);
+		constant offset_voltage : unsigned(upper_limit downto 0) := x"0300";--3V 
+
+		constant max_speed : unsigned(upper_limit downto 0) := x"2800";--40km/h
+		constant battery_voltage : unsigned(upper_limit downto 0) := x"2400";--36V
+
+		constant max_temperature : unsigned(upper_limit downto 0) := x"0200";-- Celsius
+		constant offset_temperature : unsigned(upper_limit downto 0) := x"0200";-- 80
+		constant offset_tmp_voltage : unsigned(upper_limit downto 0) := x"0300";--10V 
 		
-		signal req_speed_motor : unsigned(7 downto 0);
+		constant period_trigger : unsigned(upper_limit downto 0) := x"01fe";
+		
+		signal req_speed_motor : unsigned(upper_limit downto 0);
 		signal motor_control_setup : type_motor_control_setup;
 		signal motor_transistors : type_motor_transistors;
 		
-		signal speed : unsigned(15 downto 0)
+		signal speed : unsigned(15 downto 0);
 
-		signal out_reg : signed(IntPart + FracPart -1  downto 0);
-		signal modified_reg : signed(IntPart + FracPart -1  downto 0);
-		signal quot : unsigned(IntPart + FracPart -1  downto 0);
+		signal in_temperature_reg: signed(upper_limit  downto 0);
+		signal out_temperature_reg: signed(upper_limit  downto 0);
 
-		signal mul_a : signed(31  downto 0);
+		signal in_reg : signed(upper_limit  downto 0);
+		signal out_reg : signed(upper_limit  downto 0);
+		signal quot : unsigned(upper_limit downto 0);
+
+		signal mul_a : unsigned(31  downto 0);
 		signal mul_b : unsigned(31  downto 0);
 		signal mul_out : unsigned(31  downto 0);
+		
+		--signal mul_a_2 : unsigned(31  downto 0);
+		--signal mul_b_2 : unsigned(31  downto 0);
+		--signal mul_out_2 : unsigned(31  downto 0);
+		
+		signal divisor : unsigned(upper_limit  downto 0);
+		
+		constant clk_freq : integer := 1000000;
+		constant periods_per_sec : integer := 10;
+
+
 begin	
 	
 
+			
+	module_mul_1: mul
+	generic map(
+			 IntPart =>16,
+			 FracPart => 16
+		 )
+		port map (
+			A => mul_a,
+			B => mul_b,
+			outMul => mul_out);
+
+--	module_mul_2: mul
+--	generic map(
+--			 IntPart =>16,
+--			 FracPart => 16
+--		 )
+--		port map (
+--			A => mul_a_2,
+--			B => mul_b_2,
+--			outMul => mul_out_2);
 	
 	module_poly: poly
 	port map (
@@ -176,8 +237,8 @@ begin
 
 	speed_impulse_func : speed_impulse 
 	generic map ( 
-		 main_clock =>1000000,
-		 work_period =>10
+		 main_clock =>clk_freq,
+		 work_period =>periods_per_sec
 		)
 				
 		port map(
@@ -185,9 +246,34 @@ begin
 				clk => clk,	
 				
 				i_impulse => impulse,
-				
 				o_speed => speed
 				);
+
+	temperature_pd_module : pd 
+	generic map (
+		 	IntPart => IntPart,
+			FracPart => FracPart )
+		
+	port map (
+			res =>res,
+			clk =>clk,
+			i_enable =>enable_pid,
+			i_val => in_temperature_reg,
+			o_reg => out_temperature_reg
+			);
+
+	motor_pid_module : pid 
+	generic map (
+		 	IntPart => IntPart,
+			FracPart => FracPart )
+		
+	port map (
+			res =>res,
+			clk =>clk,
+			i_enable =>enable_pid,
+			i_val => in_reg,
+			o_reg => out_reg
+			);
 
 	trigger_func : trigger
 	generic map (
@@ -209,7 +295,7 @@ begin
 		port map( 
 				res => res, 	
 				clk => clk,		
-				i_req_speed => req_speed_motor,
+				i_req_speed => req_speed_motor(7 downto 0),
 				i_work_wave => out_trigger,
 				i_motor_control_setup => motor_control_setup,
 		
@@ -218,44 +304,30 @@ begin
 
 	module_div: div
 		generic map(
-		 size => 16)
+		 size => IntPart + FracPart)
 		port map (
 		res => res,
 		clk => clk,
 		i_enable => enable_div,
-		i_divisor => to_unsigned(modified_reg),
-		i_divident	=> battery_voltage,
+		i_divisor => divisor,
+		i_divident	=> mul_out(31 downto 16),
 		o_valid => valid,
 		o_quotient => quot);
-	
-	module_mul: mul
-	generic map(
-			 IntPart =>16,
-			 FracPart => 16
-		 )
-		port map (
-			A => mul_a,
-			B => mul_b,
-			outMul => mul_out);
-			
+
 	process(clk)
 		variable uart_sized : boolean := False; 
 	
-		
-		type type_run_motor_state is ( run_motor_get_speed, run_motor_get_pulse_width, execute_run_motor );
+		type type_regulator_state is ( regulator_idle,regulator_speed_check,regulator_init,regulator_valid, regulator_processing_output, regulator_initiate_trigger );
 			
-
 		constant config_register_h : unsigned(7 downto 0) := "01000100";
 		constant config_register_l : unsigned(7 downto 0) := "01100011";
-		constant short_break : integer := 50;
+
 		variable cnt : integer := 0;			
 		variable val_cnt : integer  range 7 downto 0 := 0;
 
-		
-		variable run_motor_state : type_run_motor_state;
-		variable uart_dev_status : type_uart_dev_status  := (False,False,False);
-		
-		constant glob_clk_denom : integer := 1000;
+		variable regulator_state : type_regulator_state := regulator_idle;
+
+		constant glob_clk_denom : integer := clk_freq/periods_per_sec;
 		constant send_motor_data_wait : integer := 500;
 		variable glob_clk_counter : integer := 0;
 		variable glob_small_clk_counter : integer range glob_clk_denom downto 0  := 0;
@@ -268,38 +340,104 @@ begin
 		
 		variable last_hal_data : std_logic_vector(2 downto 0);
 	
-		
+		variable modified_reg : signed(upper_limit  downto 0);
+		variable modified_temp_reg : signed(upper_limit  downto 0);
 	begin
 
-	
-	modified_reg := out_reg + offset_voltage;
-	if modified_reg < 0 then  
-		modified_reg := (others => '0');
-	elsif modified_reg > battery_voltage then
-		modified_reg := battery_voltage;
-	end if;
-	
 
-	
-	
 		if rising_edge(clk)  then
 			if res = '0' then
-
+				pulse_trigger <= (others => '0');
+				regulator_state := regulator_idle;
 			else
-				if impulse = '1' then
-					impulse <= '0';
-				elsif i_hal_data /= last_hal_data then
-					last_hal_data := i_hal_data;
-					impulse <= '1';
-				end if; 
+				if i_control_box_setup.enable = '1' then
+					if cnt = glob_clk_denom then
+						cnt := 0;
+						regulator_state := regulator_speed_check; 
+	
+					else 
+						cnt := cnt + 1;
+					end if;
+						
+	
+					if regulator_state = regulator_speed_check  then
+						mul_a <= (others => '0');
+						mul_b <= (others => '0');
+						mul_a(23 downto 8) <= max_speed;
+						mul_b(15 downto 8) <= i_req_speed;
+						
+						regulator_state := regulator_init;
+					elsif regulator_state = regulator_init then
+
+						in_temperature_reg <= signed(poly_temperature) + signed(max_temperature);
+						
+						req_speed_motor <= mul_out(23 downto 8 );
+						in_reg <= signed(mul_out(23 downto 8 )) - signed(speed);
+						
+						if enable_pid = '1' then
+							regulator_state := regulator_valid;
+							enable_pid <= '0';
+						else
+							enable_pid <= '1';
+						end if;
+						
+					elsif regulator_state = regulator_valid then
+						modified_reg := out_reg + signed(offset_voltage);
+						modified_temp_reg := out_temperature_reg + signed(offset_tmp_voltage);
+
+						if modified_temp_reg < 0 then  
+							modified_temp_reg := (others => '0');
+						elsif modified_temp_reg > signed(battery_voltage) then
+							modified_temp_reg := signed(battery_voltage);
+						end if;
+												
+						if modified_reg < 0 then  
+							modified_reg := (others => '0');
+						elsif modified_reg > signed(battery_voltage) then
+							modified_reg := signed(battery_voltage);
+						end if;
+						
+						if modified_temp_reg < modified_reg then
+							mul_a(31 downto 16) <= period_trigger;
+							mul_b(23 downto 8) <= unsigned(modified_temp_reg);
+							
+						else
+							mul_a(31 downto 16) <= period_trigger;
+							mul_b(23 downto 8) <= unsigned(modified_reg);
+							
+						end if;
+						
+						divisor <= (others => '0');
+						divisor(7 downto 0) <= battery_voltage(15 downto 8);
+						enable_div <= '1';
+						regulator_state := regulator_processing_output;
+					elsif regulator_state = regulator_processing_output then
+						if enable_div = '1' then
+							 enable_div <= '0';
+						end if;
+						if valid = '1' then
+							regulator_state := regulator_initiate_trigger;
+						end if;
+							
+					elsif regulator_state = regulator_initiate_trigger then
+						pulse_trigger <= quot;
+						regulator_state := regulator_idle;
+					end if;
+			
 				
-				
+					if impulse = '1' then
+						impulse <= '0';
+					elsif i_hal_data /= last_hal_data then
+						last_hal_data := i_hal_data;
+						impulse <= '1';
+					end if; 
+				end if;
+					
 			end if;
 
 		end if;
 
 	end process;
-	
 
 
 	
---
+++ logic/tb/control_box_tb.vhd
@@ -0,0 +1,96 @@
+
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+use work.interface_data.all;
+use ieee.numeric_std.all;
+use work.motor_auxiliary.all;
+
+entity control_box_tb is
+end control_box_tb;
+
+architecture t_behaviour of control_box_tb is
+
+		
+		component control_box is
+		port(
+				clk : in  std_logic;
+				res : in  std_logic;
+				
+				i_temp_transistors : in unsigned(15 downto 0);
+				i_req_speed : in unsigned(7 downto 0);
+				i_req_temperature : in unsigned(7 downto 0);
+				i_control_box_setup : in type_control_box_setup;
+				i_hal_data : in std_logic_vector(2 downto 0);
+				o_motor_transistors : out type_motor_transistors
+				);
+		end component control_box;
+	 
+		constant clk_period : time := 1 us;
+
+	 	signal clk : std_logic;
+	 	signal res : std_logic;
+	 
+		constant hal_period : time := 20 ms;
+
+		signal temp_transistors : unsigned(15 downto 0);
+		signal req_temperature : unsigned(7 downto 0);
+		signal req_speed : unsigned(7 downto 0) := x"7f";
+		signal control_box_setup : type_control_box_setup;
+		signal motor_transistors : type_motor_transistors;
+		signal hal_data : std_logic_vector(2 downto 0);
+		
+begin
+		
+			module_control_box: control_box
+			port map (
+						res => res,	
+						clk => clk,	
+						
+						i_temp_transistors => temp_transistors,
+						i_req_speed => req_speed,
+						i_req_temperature => req_temperature,
+						i_control_box_setup => control_box_setup,
+						i_hal_data => hal_data,
+						o_motor_transistors => motor_transistors 
+					);
+			
+		process	
+		begin
+				
+	
+
+				case hal_data is
+						when "101" =>  
+							hal_data<="100";
+						when "100"  =>  
+							hal_data<="110";
+						when "110"  =>  
+							hal_data<="010";
+						when "010"  =>  
+							hal_data<="011";
+						when "011"  =>  
+							hal_data<="001";
+						when "001"  =>  
+							hal_data<="101";
+						when others => 
+							hal_data<="100";
+						end case;
+
+			
+				wait for hal_period;
+				
+		end process;
+		--
+		clk_process:
+		process
+		begin
+			clk  <=  '0';
+			wait  for clk_period/2;
+			clk  <=  '1';
+			wait  for clk_period/2;
+		end  process;
+
+
+end t_behaviour;
